42358bf528269cefec29884b5a52909821c461ce
Martin Gondermann <magicmonty@pagansoft.de>
2014-01-30 20:09:42 +0100

    Added prebuild script

---
+++ prebuild.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+echo "deb http://ppa.launchpad.net/ermshiperete/monodevelop-beta/ubuntu precise main" > /etc/apt/sources.list.d/monodevelop-beta
+gpg --ignore-time-conflict --no-options --no-default-keyring --secret-keyring /etc/apt/secring.gpg --trustdb-name /etc/apt/trustdb.gpg --keyring /etc/apt/trusted.gpg --keyserver keyserver.ubuntu.com --recv 6F242C166A1B440BA3C43CBD48B6803E839ECBBE
+apt-get update
+apt-get install mono-devel mono-gmcs nunit-console monodevelop-current

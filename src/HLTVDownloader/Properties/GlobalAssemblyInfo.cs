0fc0e4c166c6dd59ef09f2904ced6f174da6c5aa
Martin Gondermann <magicmonty@pagansoft.de>
2015-10-11 17:45:40 +0200

    Updated Version information

---
+++ src/HLTVDownloader/Properties/GlobalAssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyCompany("PaganSoft")]
+[assembly: AssemblyCopyright("(c) 2015 by Martin Gondermann")]
+[assembly: AssemblyProduct("HLTVDownloader")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

9bbcdf32551a61703d9a7d1c2846fac5434ec4bf
Martin Gondermann <magicmonty@pagansoft.de>
2015-10-12 23:56:00 +0200

    Bump Version

--- src/HLTVDownloader/Properties/GlobalAssemblyInfo.cs
+++ src/HLTVDownloader/Properties/GlobalAssemblyInfo.cs
@@ -1,9 +1,9 @@
-﻿using System.Reflection;
+using System.Reflection;
 
 [assembly: AssemblyCompany("PaganSoft")]
 [assembly: AssemblyCopyright("(c) 2015 by Martin Gondermann")]
 [assembly: AssemblyProduct("HLTVDownloader")]
 
-[assembly: AssemblyVersion("1.0.0.1")]
-[assembly: AssemblyInformationalVersion("1.0.0.1")]
-[assembly: AssemblyFileVersion("1.0.0.1")]
+[assembly: AssemblyVersion("1.0.0.2")]
+[assembly: AssemblyInformationalVersion("1.0.0.2")]
+[assembly: AssemblyFileVersion("1.0.0.2")]
